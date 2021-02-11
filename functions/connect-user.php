e7eecaa9969d7ca2c06ebfdb812ca89f44c964a4
David-HUMBLOT <humblot.david1@gamil.com>
2021-02-11 21:38:54 +0400

    suite

---
+++ functions/connect-user.php
@@ -0,0 +1,25 @@
+<!-- CONNETION USER WITH VALIDATION FORM AND SECURITY David -->
+<!-- NE PAS OUBLIER DE LANCER UNE SESSION ET DE STOCKER DES INFOS EN VARIABLES DE SESSION -->
+
+
+<?php
+
+echo 'hors de la finction connecion <br/>';
+function connect_user(){
+    echo 'début de la function de connection <br/>';
+
+    /******************************************
+     * CONNECTION A LA BDD (attention : on a l include qui apel la fonction de connection depuis connect-bdd.php) *
+     ******************************************/
+
+
+
+
+
+
+
+
+// fin function user
+}
+
+?>
