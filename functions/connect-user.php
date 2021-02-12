26d670af84dd99254c80944fac47bd15df09adcc
David-HUMBLOT <humblot.david1@gamil.com>
2021-02-12 10:24:01 +0400

    ok

--- functions/connect-user.php
+++ functions/connect-user.php
@@ -3,57 +3,45 @@
 
 
 <?php
-
-echo 'hors de la finction connecion <br/>';
+echo 'hors de la fonction connecion <br/>';
 function connect_user()
 {
     echo 'début de la function de connection <br/>';
-
     /******************************************
      * CONNECTION A LA BDD (attention : on a l include qui apel la fonction de connection depuis connect-bdd.php) *
      ******************************************/
-
-
     require_once('bdd-connect.php');
     connectPdoBdd();
     echo 'Connection à la base de donnée OK <br/>';
-
-
     /******************************************
      * INITIALISATION des variables *
      ******************************************/
-
     echo ' Entrer de fonction connect-user <br/>';
     echo ' Inititialisation varibles GLOBAL  <br/>Initialisation  du tableaux des erreurs (IN FONCTIONS)  <br/>';
-
     // NOUS SERT PAR EXEMPLE A SORTIR LES INFORMATIOSN DU TABLEAUX DES ERREURS DE LA FONCTION
-    global $errors, $email, $password_connect, $pdo ,$db;
-
+    global $errors, $email, $password_connect, $pdo;
     // INITIALISATION DES VARIABLES DONT CEUX PAR DEFAUT AFIN DE LES TRAITER AVANT REQUETE DE RECUPERATION EN BASE DE DONNEE 
-
     $email = "";
     $password_connect = "";
     $errors = array();
-
-
+    //TRAITEMENT DES POST
     echo 'Traitement des champs saisie <br/>';
     if (isset($_POST['connection'])) {
         $email = trim($_POST['email']);
         $password_connect = trim($_POST['password-connect']);
-
-
+        //TRAITEMENT DES CHAMPS VIDES
         echo 'Traitement des champs vides <br/>';
         if (empty($email)) {
             array_push($errors, "Saisir une adresse email !");
         }
-        // if (empty($password_connnect)) {
-        //     array_push($errors, "Mot de passe requis");
-        // }
-
+        // evitre l evoie du caractere ESPACE car le considere toujours comme un champs vide
+        if (empty($password_connnect)) {
+            array_push($errors, "Mot de passe requis");
+        }
+        // SI LES CHAMPS SONT REMPLIE ON VERIFIE LES INFOS SAISIE AVEC LA BDD
         var_dump($errors);
         echo ' vérification des conditions <br/>';
         if (empty($errors)) {
-            
             echo 'Requete de recherche en bdd. <br/>';
             $pdo =  connectPdoBdd();
             $reqt  = "SELECT COUNT(*) AS nbr FROM  `users` WHERE  email = '$email' LIMIT 1";
@@ -62,15 +50,13 @@ function connect_user()
             $doublonEmail = $reqEmail->fetch();
             echo 'Vérification du mot de passe et de l\' email. <br/>';
             if ($doublonEmail) { // email existant
-
                 // VERIFICATION DES CHAMPS SAISIE AVEC LES MATCH EN BDD
                 // VERIFICATION DES CORRESPONDANCE DES MOTS DE PASSE (saisie à l'input et présente en bdd)
                 // utilisation de la fonction password_verify qui compart le hasf password en bdd avec le mot de passe saisie à l'input lors de la connection
                 // password_verify entre $password_connect et $doublonEmail['PASSWORD et non pas PASSWORD-CONNECT]. PASSWORD car cela correspond a commebnt il est nommé en bdd sur les ligne.
-
-        
-                if ($doublonEmail['email'] === $email && password_verify($password_connect, $doublonEmail['password']))  {
-                  echo 'compte trouvé en bdd ok <br/>';
+                if ($doublonEmail['email'] === $email && password_verify($password_connect, $doublonEmail['password'])) {
+                    echo 'compte trouvé en bdd ok <br/>';
+                    // A CE STADE SI LE COMPTE EST TROUVER ALORS ON RECUPERE LES INFORMATIONS POUR LES STOCER EN SESSION. SERVIRA NOTAMENT POUR LE FORMULAIRE DE MODIFICATION DU COMPTE ET AUSSI POUR DEMARRER UNE SESSION UNE FOIS L UTILISATEUR CONNECTER
                 }
             } else { // email n'existe pas
                 array_push($errors, "Votre compte n\' existe pas ! <br/>!");
