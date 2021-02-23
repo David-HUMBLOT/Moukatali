a5ae0e5bf8640a94da28c5f0b757bdb18b01812f
David-HUMBLOT <humblot.david1@gamil.com>
2021-02-23 20:42:02 +0400

    ok

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '16c3fd0f184ecae09557534a6c3eb4bbbd175e82',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '16c3fd0f184ecae09557534a6c3eb4bbbd175e82',
+    ),
+  ),
+);
