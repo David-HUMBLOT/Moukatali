15728fa86a47ca933ed14cd41d34ba069e6fb57a
David-HUMBLOT <humblot.david1@gamil.com>
2021-02-23 19:58:19 +0400

    ok

--- src/formUser/connection.php
+++ src/formUser/connection.php
@@ -217,23 +217,7 @@ require_once('../../functions/connect-user.php');
 <?php  }  ?>
 
 
-                <!-- MESSAGE ERREUR DE CONNECTION-->
-                <?php if (count($errors) > 0) : ?>
-                    <div class="alert alert-danger" role="alert">
-                        <?php foreach ($errors as $error) : ?>
-                            <p><?php echo $error ?></p>
-                        <?php endforeach; ?>
-                    </div>
-                <?php endif ?>
-
-                <!-- MESSAGE CONFIRMATION CONNECTION AVANT REDIRECTION -->
-                <?php if (count($success_connect) > 0) : ?>
-                    <div class="alert alert-success" role="alert">
-                        <?php foreach ($success_connect as $success_connects) : ?>
-                            <p><?php echo $success_connects ?></p>
-                        <?php endforeach; ?>
-                    </div>
-                <?php endif ?>
+           
 
 
 
